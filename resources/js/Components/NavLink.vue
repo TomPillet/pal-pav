d09579128aee2dabdbc0712b077db7617b3e0993
Tom <tom963741@gmail.com>
2023-07-30 21:39:44 +0200

    feat: first navbar layout

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -5,12 +5,13 @@ import { Link } from '@inertiajs/vue3';
 const props = defineProps<{
     href: string;
     active?: boolean;
+    bgColor: string;
 }>();
 
 const classes = computed(() =>
     props.active
-        ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-        : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+        ? `block w-10/12 m-auto py-2 text-center text-white text-lg font-semibold rounded-full hover:shadow-md hover:shadow-${props.bgColor} bg-${props.bgColor}`
+        : `block w-10/12 m-auto py-2 text-center text-black text-lg font-semibold rounded-full hover:shadow-md`
 );
 </script>
 
---
+++ resources/js/Components/Navbar.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import NavLink from "./NavLink.vue";
+
+defineProps<{
+  canLogin?: boolean;
+  canRegister?: boolean;
+}>();
+</script>
+
+<template>
+  <div class="bg-white w-60 h-screen drop-shadow-2xl absolute flex flex-col">
+    <div class="basis-1/12 flex flex-col justify-end">
+        <img class="mx-auto" src="storage/images/logo-default.svg" alt="Logo PalPav" />
+    </div>
+
+    <div class="my-auto">
+        <ul class="block flex-col">
+            <li class="my-12">
+                <NavLink :href="'#'" :bgColor="'red'" :active="false">
+                    <font-awesome-icon :icon="['fas', 'torii-gate']" />
+                    Animes & mangas
+                </NavLink>
+            </li>
+            <li class="my-12">
+                <NavLink :href="'#'" :bgColor="'passion'" :active="true">
+                    <font-awesome-icon :icon="['fas', 'clapperboard']" />
+                    Films & séries
+                </NavLink>
+            </li>
+            <li class="my-12">
+                <NavLink :href="'#'" :bgColor="'bordeaux'" :active="false">
+                    <font-awesome-icon :icon="['fas', 'book']" />
+                    Livres
+                </NavLink>
+            </li>
+        </ul>
+    </div>
+
+    <div class="basis-2/12 flex flex-col justify-end ">
+        <ul class="flex flex-row justify-evenly mb-12 px-4">
+            <li class="rounded-full hover:shadow-md">
+                <a href="#">
+                    <font-awesome-icon class="text-gray text-2xl p-4" :icon="['fas', 'user']" />
+                </a>
+            </li>
+            <li class="rounded-full hover:shadow-md">
+                <a href="#">
+                    <font-awesome-icon class="text-gray text-2xl p-4" :icon="['fas', 'bell']" />
+                </a>
+            </li>
+        </ul>
+    </div>
+  </div>
+</template>
+
+<style></style>

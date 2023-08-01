f3e04f20749366305f9c97501d4869b6422247d1
Tom <tom963741@gmail.com>
2023-08-02 01:50:31 +0200

    feat: create layouts files for each subject

---
+++ resources/js/Layouts/AnimesAndMangasLayout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="w-full h-full">Animes and mangas</div>
+
+</template>
+
+<style></style>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { Head, Link } from '@inertiajs/vue3';
 import Navbar from '../Components/Navbar.vue';
+import AnimesAndMangasLayout from '../Layouts/AnimesAndMangasLayout.vue';
+import BooksLayout from '../Layouts/BooksLayout.vue';
+import FilmsAndSeriesLayout from '../Layouts/FilmsAndSeriesLayout.vue';
 
 defineProps<{
     canLogin?: boolean;
@@ -17,7 +20,9 @@ defineProps<{
             <Navbar canLogin="canLogin" canRegister="canRegister" :activeSpace="uri" />
         </div>
         <div class="grow bg-white h-screen">
-
+            <AnimesAndMangasLayout v-if="uri === 'animes&mangas'"></AnimesAndMangasLayout>
+            <BooksLayout v-if="uri === 'books'"></BooksLayout>
+            <FilmsAndSeriesLayout v-if="uri === 'films&series'"></FilmsAndSeriesLayout>
         </div>
     </div>
 

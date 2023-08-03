a60feacad3dc981dda4a6aec580a2ab2d4099af3
Tom <tom963741@gmail.com>
2023-08-03 19:09:45 +0200

    add swiper interface in BooksLayout + swiper spin for each slide

---
+++ resources/js/Components/SwiperLoader.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref, defineProps } from 'vue';
+
+defineProps<{
+    duration: string;
+    width: string;
+    height: string;
+}>();
+</script>
+
+<template>
+    <div class="loader">
+        <svg xmlns="http://www.w3.org/2000/svg" width="124" height="124" viewBox="0 0 124 124">
+            <circle class="circle-loader" cx="62" cy="62" r="48" fill="none" stroke="#201E24" stroke-width="16px" stroke-opacity="0.5"></circle>
+            <circle class="circle" cx="62" cy="62" r="48" fill="none" stroke="#fff" stroke-width="20px"></circle>
+        </svg>
+    </div>
+</template>
+
+<style>
+.loader {
+  width: v-bind(width);
+  height: v-bind(height);
+}
+
+.loader svg {
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+
+.circle {
+  transform: rotate(-90deg);
+  transform-origin: center;
+  stroke-dasharray: 380;
+  stroke-dashoffset: 380;
+  animation: ease-in-out infinite forwards circle_stroke;
+  animation-duration: v-bind(duration);
+}
+
+@keyframes circle_stroke {
+  0% {
+    stroke-dashoffset: 380;
+  }
+
+  80%, 85% {
+    stroke-dashoffset: 0;
+  }
+  100% {
+    stroke-dashoffset: -370;
+  }
+}
+</style>
--- resources/js/Layouts/BooksLayout.vue
+++ resources/js/Layouts/BooksLayout.vue
@@ -1,9 +1,43 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/autoplay';
 
+import SwiperLoader from '../Components/SwiperLoader.vue';
+
+const delay = 5000;
+const slideSpeed = 500;
+
+const modules = [Autoplay];
+const slider = ref(null);
+const onSwiper = (swiper) => {
+    slider.value = swiper;
+    console.log(slider.value.params)
+};
 </script>
 
 <template>
-    <div class="w-full h-full">Books</div>
+
+    <div class="block h-60 relative">
+        <SwiperLoader :class="'absolute z-50 bottom-0 right-0 mb-2 mr-2'" :duration="delay+slideSpeed+'ms'" :width="'48px'" :height="'48px'"></SwiperLoader>
+
+        <swiper
+            @swiper="onSwiper"
+            :allowTouchMove="false"
+            :class="'h-full'"
+            :modules="modules"
+            :slides-per-view="1"
+            :speed="slideSpeed"
+            :autoplay="{ delay: delay, disableOnInteraction: false }"
+        >
+            <swiper-slide :class="'bg-orange-200'">Slide 1</swiper-slide>
+            <swiper-slide :class="'bg-blue-200'">Slide 2</swiper-slide>
+            <swiper-slide :class="'bg-yellow-200'">Slide 3</swiper-slide>
+        </swiper>
+    </div>
+
 </template>
 
 <style></style>
